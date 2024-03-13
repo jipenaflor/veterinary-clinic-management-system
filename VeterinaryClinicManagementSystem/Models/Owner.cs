f660e245754e937d2734024493dad37958796563
jipenaflor <rome.img23@gmail.com>
2024-03-13 18:22:55 +0800

    Added models

---
+++ VeterinaryClinicManagementSystem/Models/Owner.cs
@@ -0,0 +1,25 @@
+﻿namespace VeterinaryClinicManagementSystem.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+        public ICollection<Animal> Pets { get; }
+
+        public Owner(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+            Pets = new List<Animal>();
+        }
+
+        public void AddPets(Animal pet)
+        {
+            Pets.Add(pet); ;
+        }
+    }
+}
---
+++ VeterinaryClinicManagementSystem/Models/Veterinarian.cs
@@ -0,0 +1,15 @@
+﻿namespace VeterinaryClinicManagementSystem.Models
+{
+    public class Veterinarian
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+
+        public Veterinarian(string name, string contactNumber)
+        {
+            Name = name;
+            ContactNumber = contactNumber;
+        }
+    }
+}
